4c7abc648242795e7ee072120a844fff912b58cb
Vladimir Skomorokhov <vizion84@yandex.ru>
2023-11-03 12:25:27 +0300

    Completed task 3 for chapter 16

--- 16.Chapter/examples/game/game.js
+++ 16.Chapter/examples/game/game.js
@@ -112,10 +112,48 @@ let simpleLevelPlan = `
 
 	Coin.prototype.size = new Vec(0.6, 0.6);
 
+	//const monsterSpeed = 4;
+	let Monster = class Monster {
+		constructor(pos) {
+			this.pos = pos;
+			this.monsterSpeed = 4;
+		}
+		
+		get type() { return "monster"; }
+  
+		static create(pos) {
+		  return new Monster(pos.plus(new Vec(0, -1)));
+		}
+  
+		update(time, state) {
+			let player = state.player;
+			let speed = (player.pos.x < this.pos.x ? -1 : 1) * time * this.monsterSpeed;
+			let newPos = new Vec(this.pos.x + speed, this.pos.y);
+			if (state.level.touches(newPos, this.size, "wall")) {
+				return this;
+			} else {
+				return new Monster(newPos);
+			}
+		}
+  
+		collide(state) {
+			let player = state.player;
+			if (player.pos.y + player.size.y < this.pos.y + 0.5) {
+				let filtered = state.actors.filter(a => a != this);
+				return new State(state.level, filtered, state.status);
+			} else {
+			  	return new State(state.level, state.actors, "lost");
+			}
+		}
+	};
+
+	Monster.prototype.size = new Vec(1.2, 2);
+
 	let levelChars = {
 		".": "empty", "#": "wall", "+": "lava",
 		"@": Player, "o": Coin,
-		"=": Lava, "|": Lava, "v": Lava
+		"=": Lava, "|": Lava, "v": Lava,
+		'M': Monster,
 	};
 
 	let simpleLevel = new Level(simpleLevelPlan);
